d856a423e8d5a7872f6a14f89a51fc734ffaaeb7
flinux-sudo <filipgondek78@gmail.com>
2020-11-12 18:58:42 +0100

    Initial commit

---
+++ src/com/controllingexecution/ClassifyingNumbers.java
@@ -0,0 +1,4 @@
+package com.controllingexecution;
+
+public class ClassifyingNumbers {
+}
---
+++ src/com/controllingexecution/CountingNumbers.java
@@ -0,0 +1,13 @@
+package com.controllingexecution;
+
+public class CountingNumbers {
+
+    public static void counting(){
+        for(int i=1;i<=100;i++)
+            System.out.println("number: "+i);
+    }
+    public static void main(String[] args) {
+        counting();
+    }
+
+}
