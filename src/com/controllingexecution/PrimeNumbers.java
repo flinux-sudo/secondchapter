e38f587dc5ef3a7b3d4e0c1e1598f1561e934b4e
flinux-sudo <filipgondek78@gmail.com>
2020-11-20 00:07:45 +0100

    differs a little from our version and i dont know if assigning to boundary the max value of long is what you wanted from 1),2),3).Can you explain to me with an example what it is and how to limit primitive data type to ram and not to the limitations of java mechanims from 4)

--- src/com/controllingexecution/PrimeNumbers.java
+++ src/com/controllingexecution/PrimeNumbers.java
@@ -1,7 +1,41 @@
 package com.controllingexecution;
 
 public class PrimeNumbers {
+    public static void printPrimeNumbers(int i, boolean valid){
+        if(valid) {
+            System.out.println("prime number: " + i);
+        }else{
+            System.out.println("is not prime number: " + i);
+
+        }
+    }
+
+
+    public static void primeNumbers(){
+        long boundary=Long.MAX_VALUE;
+        for(int i=2;i<=boundary;i++) {
+            boolean isPrime=true;
+            isPrime = isPrime(isPrime, i);
+            if(isPrime) {
+                printPrimeNumbers(i,isPrime);
+            }
+        }
+
+    }
+
+    public static boolean isPrime(boolean isPrime, int i) {
+        for (int j =2; j<i-1;j++) {
+            if (i % j == 0) {
+                isPrime=false;
+                printPrimeNumbers(i,isPrime);
+                break;
+            }
+        }
+        return isPrime;
+    }
+
     public static void main(String[] args) {
+        primeNumbers();
 
     }
 }
